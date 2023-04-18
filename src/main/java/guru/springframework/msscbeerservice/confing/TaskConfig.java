03536a9dadbc50978f3df0ab75f840355778d49a
Mateusz Krajewski <maatsior@gmail.com>
2023-04-18 21:01:22 +0200

    Add BrewingService

---
+++ src/main/java/guru/springframework/msscbeerservice/confing/TaskConfig.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscbeerservice.confing;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableAsync
+@EnableScheduling
+public class TaskConfig {
+
+  @Bean
+  public TaskExecutor taskExecutor() {
+    return new SimpleAsyncTaskExecutor();
+  }
+}
